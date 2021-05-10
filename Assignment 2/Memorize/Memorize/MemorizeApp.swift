bdebedee6e859243fd8d128f7afba5ce7b34dab2
peterkost <46546031+peterkost@users.noreply.github.com>
2021-05-10 13:23:19 +0300

    a2 starting point
    where a1 left off

---
+++ Assignment 2/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Peter Kostin on 2021-05-08.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(viewModel: EmojiMemoryGame())
+        }
+    }
+}
