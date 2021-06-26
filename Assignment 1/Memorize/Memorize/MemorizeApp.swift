4c0e3ff1a83eff497b834e5ecbf9eac879813851
peterkost <46546031+peterkost@users.noreply.github.com>
2021-06-26 12:05:24 +0300

    lecture 1 + 2 code

---
+++ Assignment 1/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Peter Kostin on 2021-06-25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
