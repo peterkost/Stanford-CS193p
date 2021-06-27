2510fe1b915f6df2eca36871e2c01a0bb61e84e4
peterkost <46546031+peterkost@users.noreply.github.com>
2021-06-27 12:40:08 +0300

    A2 extra credit 1 & 2

--- Assignment 2/Memorize/Memorize/Theme.swift
+++ Assignment 2/Memorize/Memorize/Theme.swift
@@ -12,15 +12,26 @@ struct Theme {
     let emojis: [String]
     let pairsOfCards: Int
     let color: String
+    
+    init(name: String, emojis: [String], pairsOfCards: Int? = nil, randomPairsOfCards: Bool = false, color: String) {
+        self.name = name
+        self.emojis = emojis
+        if randomPairsOfCards {
+            self.pairsOfCards = Int.random(in: 8..<emojis.count)
+        } else {
+            self.pairsOfCards = pairsOfCards ?? emojis.count
+        }
+        self.color = color
+    }
 }
 
 class Themes {
     let themes = [
         Theme(name: "emojis", emojis: ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "🥲", "☺️", "😊", "😇", "🙂", "🙃", "😉", "😌", "😍", "🥰", "😘", "😗", "😙", "😚", "😋", "😛", "😝"], pairsOfCards: 20, color: "yellow"),
-        Theme(name: "transport", emojis: ["✈️", "🚀", "🚗", "🚘", "🚙", "🏎", "🏍", "🚌", "🚐", "🚛", "🛳", "🚑", "🚜", "🚂", "🚄", "🚎", "🛻", "🛴", "🛵", "🚁"], pairsOfCards: 20, color: "blue"),
+        Theme(name: "transport", emojis: ["✈️", "🚀", "🚗", "🚘", "🚙", "🏎", "🏍", "🚌", "🚐", "🚛", "🛳", "🚑", "🚜", "🚂", "🚄", "🚎", "🛻", "🛴", "🛵", "🚁"], color: "blue"),
         Theme(name: "sports", emojis: ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🎾", "🏐", "🏉", "🏓", "🎳", "🏸", "🏒", "🏑", "🥍", "🏏", "⛷", "🥊", "🚙", "🏋️‍♀️", "🤺"], pairsOfCards: 20, color: "green"),
         Theme(name: "food", emojis: ["🍔", "🍟", "🍕", "🌮", "🥪", "🥗", "🌯", "🍣", "🥟", "🍤", "🍧", "🍰", "🍪", "🍫", "🥓"], pairsOfCards: 15, color: "red"),
-        Theme(name: "flags", emojis: ["🇷🇺", "🇧🇾", "🇰🇿", "🇰🇬", "🇸🇾", "🇨🇳", "🇰🇵", "🇷🇸", "🇫🇷", "🇲🇳", "🇲🇺", "🇨🇦", "🇱🇧", "🇦🇴", "🇲🇿", "🇺🇬", "🇮🇷", "🇨🇺", "🇲🇩", "🇸🇴"], pairsOfCards: 20, color: "darkPink"),
+        Theme(name: "flags", emojis: ["🇷🇺", "🇧🇾", "🇰🇿", "🇰🇬", "🇸🇾", "🇨🇳", "🇰🇵", "🇷🇸", "🇫🇷", "🇲🇳", "🇲🇺", "🇨🇦", "🇱🇧", "🇦🇴", "🇲🇿", "🇺🇬", "🇮🇷", "🇨🇺", "🇲🇩", "🇸🇴"], randomPairsOfCards: true, color: "darkPink"),
         Theme(name: "animals", emojis: ["🐵", "🐸", "🐭", "🐮", "🦆", "🦇", "🐝", "🦖", "🦞", "🦍"], pairsOfCards: 10, color: "orange")
     ]
     
