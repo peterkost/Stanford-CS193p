25b2cfed77b99e30010bf3dc5e33f74aa8d4f334
peterkost <46546031+peterkost@users.noreply.github.com>
2021-06-29 11:03:26 +0300

    generate 81 unique cards

---
+++ Assignment 3/SetGame/SetGame/SetGame.swift
@@ -0,0 +1,47 @@
+//
+//  SetGame.swift
+//  SetGame
+//
+//  Created by Peter Kostin on 2021-06-29.
+//
+
+import Foundation
+
+struct SetGame {
+    private(set) var cards = [Card]()
+    
+    init() {
+        for i in Property.allCases {
+            for j in Property.allCases {
+                for k in Property.allCases {
+                    for l in Property.allCases {
+                        cards.append(Card(property1: i, property2: j, property3: k, property4: l))
+                    }
+                }
+            }
+        }
+        
+        cards.shuffle()
+        
+        for n in 0..<12 {
+            cards[n].location = .onBoard
+        }
+    }
+    
+    struct Card {
+        let id = UUID()
+        var location =  Location.inDeck
+        let property1: Property
+        let property2: Property
+        let property3: Property
+        let property4: Property
+    }
+    
+    enum Location {
+        case inDeck, onBoard, discarded
+    }
+    
+    enum Property: CaseIterable {
+        case a, b, c
+    }
+}
